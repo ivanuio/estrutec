e1614b24299ec9c820f16a703a4be0f455532710
Ivan Ordonez <ivanord@gmail.com>
2023-09-07 07:51:16 -0500

    Initial commit before saving images

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  def index
+  end
+require 'date'
+
+  def about #this is about method
+    @about_me = "Enhancing the design of a construction project is crucial, not only to meet the client's requirements but also to improve the outcomes for all stakeholders involved in the project-construction process. My name is Paul Ordonez... and Estrutecnia is established"
+    @answer = 2023-2017 
+  end
+
+  def acerca #this is acerca method
+    @about_me = "Somos un equipo de profesionales comprometidos en brindar soluciones efectivas a nuestros clientes"
+    
+  end
+end
