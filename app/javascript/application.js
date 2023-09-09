719d1e10fdd1c58018fad505fdf5740e979b9c68
Ivan Ordonez <ivanord@gmail.com>
2023-09-08 20:03:42 -0500

    masonry without JS only Css working pretty fine GOD

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,14 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 require jquery //this solved a f problem when the dropdown menu was not working
+require jquery.turbolinks
+require masonry/jquery.masonry
+require masonry/jquery.event-drag
+require masonry/jquery.imagesloaded.min
+require masonry/jquery.infinitescroll.min
+require masonry/modernizr-transitions
+
+require masonry/box-maker
+require masonry/jquery.loremimages.min
+
+require isotope/jquery.isotope
